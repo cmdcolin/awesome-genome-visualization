701d6a7113995064fc94eed00b6e2c75a7f539ef
Colin <colin.diesh@gmail.com>
2022-01-21 14:24:06 -0700

    Updates

--- get_year.js
+++ get_year.js
@@ -3,6 +3,8 @@
 import fs from 'fs'
 import fetch from 'node-fetch'
 
+import { setTimeout } from 'timers/promises'
+
 const data = JSON.parse(fs.readFileSync('TOOLS.json', 'utf8'))
 // const d2 = Object.fromEntries(
 //   fs
@@ -11,25 +13,45 @@ const data = JSON.parse(fs.readFileSync('TOOLS.json', 'utf8'))
 //     .filter(f => !!f)
 //     .map(line => line.split(' ')),
 // )
-console.log('data', data)
 ;(async () => {
-  for (let i = 0; i < data.tools.length; i++) {
+  let timeout = 1000
+  for (let i = 0; i < data.tools.length; ) {
     const d = data.tools[i]
-    console.log({ d })
-    if (d.pub) {
-      const doi = d.pub.doi
-      const url = doi.startsWith('http') ? doi : 'https://doi.org/' + doi
-      const response = await fetch(url, {
-        headers: { Accept: 'application/json' },
-      })
-      if (!response.ok)
-        throw new Error(
-          'failed ${response.statusText} ${await response.text()}',
+
+    try {
+      if (d.pub) {
+        const doi = d.pub.doi
+        if (doi.includes('zenodo')) {
+          i++
+          continue
+        }
+        console.log(
+          i + '/' + data.tools.length,
+          'curr waittime',
+          timeout,
+          'doi',
+          doi,
         )
-      const json = await response.json()
-      d.year = json.published['date-parts'][0][0]
+        const url = doi.startsWith('http') ? doi : 'https://doi.org/' + doi
+        const response = await fetch(url, {
+          headers: { Accept: 'application/json' },
+        })
+        if (!response.ok) {
+          throw new Error(
+            `failed ${response.statusText} ${await response.text()}`,
+          )
+        }
+        const json = await response.json()
+        d.pub.year = json.published['date-parts'][0][0]
+        timeout = 1000
+        setTimeout(timeout)
+      }
+      i++
+    } catch (e) {
+      console.error('got error, retrying', e)
+      await setTimeout(timeout)
+      timeout = timeout * 2
     }
   }
+  fs.writeFileSync('TOOLS.json', JSON.stringify(data, null, 2))
 })()
-
-fs.writeFileSync('TOOLS.json', JSON.stringify(data, null, 2))
