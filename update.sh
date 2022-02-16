b6dc0fa3fbb777d98f0118007a2806e1bb97b474
Colin <colin.diesh@gmail.com>
2022-02-16 14:41:29 -0700

    Add some protein visualization tools

--- update.sh
+++ update.sh
@@ -3,5 +3,6 @@ cd awesome-genome-visualization/static;
 identify -format "%f %wx%h\n" *.jpg *.png > ../../dims.txt
 cd ../../
 node get_dimensions.js
+node get_citations.js
 node get_stars.js
 npx prettier --write TOOLS.json
