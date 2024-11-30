1282170b8dfc6087eb90c0c755d295f77354d805
Colin <colin.diesh@gmail.com>
2024-11-30 17:12:44 -0500

    Misc

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react'
 import queryString from 'query-string'
 import { tools as importedTools } from './TOOLS.json'
-import { useAppStore } from './store'
+import { Tool, useAppStore } from './store'
 
 // locals
 import InteractiveFilters from './InteractiveFilters'
@@ -82,180 +82,14 @@ export default function App() {
     .filter(t => (platform ? t.platform?.includes(platform) : true))
     .filter(t => (interactive ? t.interactive?.includes(interactive) : true))
 
-  const githubURL = 'https://github.com/cmdcolin/awesome-genome-visualization'
-
   return (
-    <div className="m-auto max-w-7xl prose">
-      <div className="flex flex-col gap-4">
-        <h1 className="text-3xl">awesome-genome-visualization</h1>
-        <p>
-          This is a companion website for the github repo{' '}
-          <Link href={githubURL}>{githubURL}</Link>
-        </p>
-        <p>
-          Also check out our twitter account{' '}
-          <Link href="https://twitter.com/awesomegenomev1">
-            @awesomegenomev1
-          </Link>
-        </p>
-        <p>Feel free to submit PRs to add more tools</p>
-
-        <p className="max-w-lg">
-          Filters:
-          <Button
-            onClick={() => {
-              store.setFilters({})
-            }}
-          >
-            Clear filters
-          </Button>
-          <Button
-            onClick={() => {
-              store.setFilters({ tag: 'General' })
-            }}
-          >
-            General-purpose genome browsers
-          </Button>
-          <Button
-            onClick={() => {
-              store.setFilters({ tag: 'Comparative' })
-            }}
-          >
-            Synteny/comparative browsers
-          </Button>
-          <Button
-            onClick={() => {
-              store.setFilters({ tag: 'Dotplot' })
-            }}
-          >
-            Dotplot viewer
-          </Button>
-          <Button
-            onClick={() => {
-              store.setFilters({ tag: 'MSA' })
-            }}
-          >
-            MSA viewer
-          </Button>
-          <Button
-            onClick={() => {
-              store.setFilters({ tag: 'Graph' })
-            }}
-          >
-            Graph genome
-          </Button>
-          <Button
-            onClick={() => {
-              store.setFilters({ tag: 'Text based' })
-            }}
-          >
-            Text based
-          </Button>
-        </p>
-        <p className="max-w-lg">
-          Sorts:
-          <Button
-            onClick={() => {
-              store.setSort({ latest: true })
-            }}
-          >
-            Recently added
-          </Button>
-          <Button
-            onClick={() => {
-              store.setSort({ latest: false })
-            }}
-          >
-            Least recently added
-          </Button>
-          <Button
-            onClick={() => {
-              store.setSort({ year: -1 })
-            }}
-          >
-            Year (dec)
-          </Button>
-          <Button
-            onClick={() => {
-              store.setSort({ year: 1 })
-            }}
-          >
-            Year (asc)
-          </Button>
-          <Button
-            onClick={() => {
-              store.setSort({ citations: -1 })
-            }}
-          >
-            Number citations (dec)
-          </Button>
-          <Button
-            onClick={() => {
-              store.setSort({ citations: 1 })
-            }}
-          >
-            Number citations (asc)
-          </Button>
-          <Button
-            onClick={() => {
-              store.setSort({ stars: -1 })
-            }}
-          >
-            Github stars (dec)
-          </Button>
-          <Button
-            onClick={() => {
-              store.setSort({ stars: 1 })
-            }}
-          >
-            Github stars (asc)
-          </Button>
-        </p>
-
-        <p>
-          Selection:
-          <Button
-            onClick={() => {
-              store.setSelected()
-            }}
-          >
-            Clear selection
-          </Button>
-        </p>
-
-        <div>
-          <TagFilters tools={tools} />
-          <LanguageFilters tools={tools} />
-          <PlatformFilters tools={tools} />
-          <InteractiveFilters tools={tools} />
-        </div>
-        <div className="flex gap-2">
-          <span>
-            <label htmlFor="grid">Grid</label>
-            <input
-              id="grid"
-              type="radio"
-              className="radio radio-sm"
-              checked={mode === 'grid'}
-              onChange={() => {
-                store.setMode('grid')
-              }}
-            />
-          </span>
-          <span>
-            <label htmlFor="list">List</label>
-            <input
-              id="list"
-              type="radio"
-              className="radio radio-sm"
-              checked={mode === 'list'}
-              onChange={() => {
-                store.setMode('list')
-              }}
-            />
-          </span>
-        </div>
-      </div>
+    <div className="m-auto max-w-7xl flex flex-col gap-4">
+      <Header />
+      <FilterButtons />
+      <SortButtons />
+      <ClearSelection />
+      <FilterSelectors tools={tools} />
+      <GridSelector />
 
       {mode === 'list' ? (
         <div className="mt-6 flex flex-col space-y-8 overflow-hidden">
@@ -270,6 +104,203 @@ export default function App() {
         Note: if you would like your tool removed or screenshot removed (for
         copyright purposes for example) let me know
       </p>
+    </div>
+  )
+}
+
+function GridSelector() {
+  const store = useAppStore()
+  const { mode } = store
+  return (
+    <div className="flex gap-2">
+      <label htmlFor="grid">Grid</label>
+      <input
+        id="grid"
+        type="radio"
+        className="radio radio-sm"
+        checked={mode === 'grid'}
+        onChange={() => {
+          store.setMode('grid')
+        }}
+      />
+      <label htmlFor="list">List</label>
+      <input
+        id="list"
+        type="radio"
+        className="radio radio-sm"
+        checked={mode === 'list'}
+        onChange={() => {
+          store.setMode('list')
+        }}
+      />
+    </div>
+  )
+}
+
+function SortButtons() {
+  const store = useAppStore()
+  return (
+    <p className="max-w-lg">
+      Sorts:
+      <Button
+        onClick={() => {
+          store.setSort({ latest: true })
+        }}
+      >
+        Recently added
+      </Button>
+      <Button
+        onClick={() => {
+          store.setSort({ latest: false })
+        }}
+      >
+        Least recently added
+      </Button>
+      <Button
+        onClick={() => {
+          store.setSort({ year: -1 })
+        }}
+      >
+        Year (dec)
+      </Button>
+      <Button
+        onClick={() => {
+          store.setSort({ year: 1 })
+        }}
+      >
+        Year (asc)
+      </Button>
+      <Button
+        onClick={() => {
+          store.setSort({ citations: -1 })
+        }}
+      >
+        Number citations (dec)
+      </Button>
+      <Button
+        onClick={() => {
+          store.setSort({ citations: 1 })
+        }}
+      >
+        Number citations (asc)
+      </Button>
+      <Button
+        onClick={() => {
+          store.setSort({ stars: -1 })
+        }}
+      >
+        Github stars (dec)
+      </Button>
+      <Button
+        onClick={() => {
+          store.setSort({ stars: 1 })
+        }}
+      >
+        Github stars (asc)
+      </Button>
+    </p>
+  )
+}
+
+function FilterButtons() {
+  const store = useAppStore()
+  return (
+    <p className="max-w-lg">
+      Filters:
+      <Button
+        onClick={() => {
+          store.setFilters({})
+        }}
+      >
+        Clear filters
+      </Button>
+      <Button
+        onClick={() => {
+          store.setFilters({ tag: 'General' })
+        }}
+      >
+        General-purpose genome browsers
+      </Button>
+      <Button
+        onClick={() => {
+          store.setFilters({ tag: 'Comparative' })
+        }}
+      >
+        Synteny/comparative browsers
+      </Button>
+      <Button
+        onClick={() => {
+          store.setFilters({ tag: 'Dotplot' })
+        }}
+      >
+        Dotplot viewer
+      </Button>
+      <Button
+        onClick={() => {
+          store.setFilters({ tag: 'MSA' })
+        }}
+      >
+        MSA viewer
+      </Button>
+      <Button
+        onClick={() => {
+          store.setFilters({ tag: 'Graph' })
+        }}
+      >
+        Graph genome
+      </Button>
+      <Button
+        onClick={() => {
+          store.setFilters({ tag: 'Text based' })
+        }}
+      >
+        Text based
+      </Button>
+    </p>
+  )
+}
+
+function Header() {
+  const githubURL = 'https://github.com/cmdcolin/awesome-genome-visualization'
+  return (
+    <div>
+      <h1 className="text-3xl">awesome-genome-visualization</h1>
+      <p>
+        This is a companion website for the github repo{' '}
+        <Link href={githubURL}>{githubURL}</Link>
+      </p>
+      <p>
+        Also check out our twitter account{' '}
+        <Link href="https://twitter.com/awesomegenomev1">@awesomegenomev1</Link>
+      </p>
+      <p>Feel free to submit PRs to add more tools</p>
+    </div>
+  )
+}
+
+function ClearSelection() {
+  const store = useAppStore()
+  return (
+    <p>
+      Selection:
+      <Button
+        onClick={() => {
+          store.setSelected()
+        }}
+      >
+        Clear selection
+      </Button>
+    </p>
+  )
+}
+
+function FilterSelectors({ tools }: { tools: Tool[] }) {
+  return (
+    <div>
+      <TagFilters tools={tools} />
+      <LanguageFilters tools={tools} />
+      <PlatformFilters tools={tools} />
+      <InteractiveFilters tools={tools} />
     </div>
   )
 }
