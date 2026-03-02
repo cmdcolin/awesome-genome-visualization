2752ede299b4fe3df6900d22e57bcb1e53a05534
Colin <colin.diesh@gmail.com>
2026-03-02 15:14:06 -0500

    Updates

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,9 @@ import App from './App'
 
 import './index.css'
 import { StrictMode } from 'react'
-import { NuqsAdapter } from 'nuqs/adapters/react'
 
 ReactDOM.createRoot(document.querySelector('#root')!).render(
   <StrictMode>
-    <NuqsAdapter>
-      <App />
-    </NuqsAdapter>
+    <App />
   </StrictMode>,
 )
